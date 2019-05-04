c1c5a8a7dd76ca1b79f61087b9e958ceec893053
vergillr <vergillrl@gmail.com>
2019-05-04 15:54:55 +0200

    Removed dependencies on flutter and flutter_svg; also, example code is now a comment block and the test folder was removed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,36 +1,40 @@
+## [1.2.1] - 2019-05-04.
+
+- Removed dependencies on flutter and flutter_svg (they were only required for the test and example code); example code is now shown as a comment block and the test code has been removed
+
 ## [1.2.0] - 2019-03-07.
 
-* Path and opacity are rounded now to the same decimals as Jdenticon's code
+- Path and opacity are rounded now to the same decimals as Jdenticon's code
 
 ## [1.1.1] - 2019-02-21.
 
-* Resolved an issue when adding padding
+- Resolved an issue when adding padding
 
 ## [1.1.0] - 2019-02-19.
 
-* Added the ability to create custom icons with the same icon builder options from Jdenticon
-* Icons now also have a grayscale saturation (in addition to the default color saturation)
+- Added the ability to create custom icons with the same icon builder options from Jdenticon
+- Icons now also have a grayscale saturation (in addition to the default color saturation)
 
 ## [1.0.5] - 2019-02-19.
 
-* Run dartfmt
+- Run dartfmt
 
 ## [1.0.4] - 2019-02-19.
 
-* Reverted one function to the old code
+- Reverted one function to the old code
 
 ## [1.0.3] - 2019-02-19.
 
-* Cleaned up code against Jdenticon's code base and tested all
+- Cleaned up code against Jdenticon's code base and tested all
 
 ## [1.0.2] - 2019-02-13.
 
-* Final change with dartfmt and README
+- Final change with dartfmt and README
 
 ## [1.0.1] - 2019-02-13.
 
-* Changed format style to conform with analysis
+- Changed format style to conform with analysis
 
 ## [1.0.0] - 2019-02-13.
 
-* First release
+- First release
--- example/example.dart
+++ example/example.dart
@@ -1,109 +1,110 @@
-import 'package:flutter/material.dart';
-import 'package:jdenticon_dart/jdenticon_dart.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// Jdenticon only has 1 function you will need: toSvg(). This function requires a message (as a String).
+// The output is a SVG string which needs to be passed to the widget SvgPicture so that it can be rendered.
+// You can set the sizing, scaling, padding and margins when the SVG is actually rendered with SvgPicture, as seen below.
+// Uncomment all comments below and run the code to see Jdenticon in action.
 
-void main() => runApp(Example());
+// import 'package:flutter/material.dart';
+// import 'package:jdenticon_dart/jdenticon_dart.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
-Widget _getCardWithIcon(String name) {
-  // Jdenticon only has 1 function you will need: toSvg(). This function requires a message (as a String).
-  // The output is a SVG string which needs to be passed to the widget SvgPicture so that it can be rendered.
-  // You can set the sizing, scaling, padding and margins when the SVG is actually rendered with SvgPicture, as seen below.
-  final String rawSvg = Jdenticon.toSvg(name);
+// void main() => runApp(Example());
 
-  return Card(
-    child: Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SvgPicture.string(
-                rawSvg,
-                fit: BoxFit.fill,
-                height: 32,
-                width: 32,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SvgPicture.string(
-                rawSvg,
-                fit: BoxFit.fill,
-                height: 32,
-                width: 32,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SvgPicture.string(
-                rawSvg,
-                fit: BoxFit.fill,
-                height: 32,
-                width: 32,
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: 12.0,
-        ),
-        SvgPicture.string(
-          rawSvg,
-          fit: BoxFit.contain,
-          height: 64,
-          width: 64,
-        ),
-        SizedBox(
-          height: 12.0,
-        ),
-        SvgPicture.string(
-          rawSvg,
-          fit: BoxFit.scaleDown,
-          height: 128,
-          width: 128,
-        ),
-        SizedBox(
-          height: 12.0,
-        ),
-        SvgPicture.string(
-          rawSvg,
-          fit: BoxFit.fitWidth,
-          width: 256,
-        ),
-        Text(
-          name,
-          textScaleFactor: 1.5,
-          style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.6),
-        ),
-      ],
-    ),
-  );
-}
+// Widget _getCardWithIcon(String name) {
+//   final String rawSvg = Jdenticon.toSvg(name);
 
-class Example extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jdenticon Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Jdenticon Example'),
-        ),
-        body: Center(
-          child: ListView(
-            children: <Widget>[
-              // Give a string like a name, message or id to get the Jdenticon; please note that Jdenticon is case-sensitive
-              _getCardWithIcon('Naomi'),
-              _getCardWithIcon('Ridge'),
-              _getCardWithIcon('Callie'),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   return Card(
+//     child: Column(
+//       children: <Widget>[
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Padding(
+//               padding: EdgeInsets.all(8.0),
+//               child: SvgPicture.string(
+//                 rawSvg,
+//                 fit: BoxFit.fill,
+//                 height: 32,
+//                 width: 32,
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsets.all(8.0),
+//               child: SvgPicture.string(
+//                 rawSvg,
+//                 fit: BoxFit.fill,
+//                 height: 32,
+//                 width: 32,
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsets.all(8.0),
+//               child: SvgPicture.string(
+//                 rawSvg,
+//                 fit: BoxFit.fill,
+//                 height: 32,
+//                 width: 32,
+//               ),
+//             ),
+//           ],
+//         ),
+//         SizedBox(
+//           height: 12.0,
+//         ),
+//         SvgPicture.string(
+//           rawSvg,
+//           fit: BoxFit.contain,
+//           height: 64,
+//           width: 64,
+//         ),
+//         SizedBox(
+//           height: 12.0,
+//         ),
+//         SvgPicture.string(
+//           rawSvg,
+//           fit: BoxFit.scaleDown,
+//           height: 128,
+//           width: 128,
+//         ),
+//         SizedBox(
+//           height: 12.0,
+//         ),
+//         SvgPicture.string(
+//           rawSvg,
+//           fit: BoxFit.fitWidth,
+//           width: 256,
+//         ),
+//         Text(
+//           name,
+//           textScaleFactor: 1.5,
+//           style: TextStyle(
+//               color: Colors.blue,
+//               fontWeight: FontWeight.bold,
+//               letterSpacing: 1.6),
+//         ),
+//       ],
+//     ),
+//   );
+// }
+
+// class Example extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Jdenticon Example',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Jdenticon Example'),
+//         ),
+//         body: Center(
+//           child: ListView(
+//             children: <Widget>[
+//               _getCardWithIcon('Naomi'),
+//               _getCardWithIcon('Ridge'),
+//               _getCardWithIcon('Callie'),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
